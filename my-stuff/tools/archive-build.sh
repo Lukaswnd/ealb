1940347eb8c4b48b0be25d3b23ec5d9de25704c6
Lukaswnd <Lukas+git@windeln.nrw>
2025-01-03 17:43:32 +0100

    try shorten some paths try reduce compile size

--- my-stuff/tools/archive-build.sh
+++ my-stuff/tools/archive-build.sh
@@ -5,8 +5,8 @@ IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD || git -C "$IDF_PATH" 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 
 archive_path="dist/arduino-esp32-libs-$1-$idf_version_string.tar.gz"
-$AR_SDK_zip_archive_path="/framework-arduinoespressif32.zip"
-pio_zip_archive_libs_path="/framework-arduinoespressif32-libs.zip"
+$AR_SDK_zip_archive_path="/fae32.zip"
+pio_zip_archive_libs_path="/fae32-libs.zip"
 pio_zip_archive_short="/build.zip"
 
 mkdir -p dist && rm -rf "$archive_path"
@@ -15,7 +15,7 @@ if [ -d "out" ]; then
 fi
 
 cd out 
-echo "Creating PioArduino framework-arduinoespressif32"
+echo "Creating PioArduino fae32"
 mkdir -p arduino-esp32/cores/esp32
 mkdir -p arduino-esp32/tools/partitions
 cp -rf ../components/arduino/tools arduino-esp32
@@ -32,6 +32,7 @@ rm -rf arduino-esp32/idf_component_examples
 rm -rf arduino-esp32/libraries/RainMaker
 rm -rf arduino-esp32/libraries/Insights
 rm -rf arduino-esp32/libraries/SPIFFS
+rm -rf arduino-esp32/libraries/PPP
 rm -rf arduino-esp32/libraries/WiFiProv
 rm -rf arduino-esp32/libraries/TFLiteMicro
 rm -rf arduino-esp32/libraries/OpenThread
@@ -42,37 +43,37 @@ rm -rf arduino-esp32/tools/gen_insights_package.py
 
 cp ../package.json arduino-esp32/package.json
 cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
-cp -rf tools/esp32-arduino-libs framework-arduinoespressif32-libs/
-cp -rf arduino-esp32/ framework-arduinoespressif32/
+cp -rf tools/esp32-arduino-libs fae32-libs/
+cp -rf arduino-esp32/ fae32/
 
 
-cp -rf tools/esp32-arduino-libs arduino-esp32/tools/
+#cp -rf tools/esp32-arduino-libs arduino-esp32/tools/
 # Use sed to replace the line
-sed -i '/^FRAMEWORK_LIBS_DIR = /c\FRAMEWORK_LIBS_DIR = join(FRAMEWORK_DIR, "tools", "esp32-arduino-libs")' "arduino-esp32/tools/pioarduino-build.py"
+#sed -i '/^FRAMEWORK_LIBS_DIR = /c\FRAMEWORK_LIBS_DIR = join(FRAMEWORK_DIR, "tools", "esp32-arduino-libs")' "arduino-esp32/tools/pioarduino-build.py"
 # Check if the sed command was successful
-if [ $? -eq 0 ]; then
-  echo "File updated successfully."
-else
-  echo "Failed to update the file."
-fi
-find "arduino-esp32/tools/esp32-arduino-libs" -type f -name "pioarduino-build.py" | while read -r FILE_PATH; do
-  # Use sed to replace the line
-  sed -i -e '/^FRAMEWORK_SDK_DIR = env.PioPlatform().get_package_dir(/,/^)/c\FRAMEWORK_SDK_DIR = join(FRAMEWORK_DIR, "tools", "esp32-arduino-libs")' "$FILE_PATH"
+#if [ $? -eq 0 ]; then
+#  echo "File updated successfully."
+#else
+#  echo "Failed to update the file."
+#fi
+#find "arduino-esp32/tools/esp32-arduino-libs" -type f -name "pioarduino-build.py" | while read -r FILE_PATH; do
+#  # Use sed to replace the line
+#  sed -i -e '/^FRAMEWORK_SDK_DIR = env.PioPlatform().get_package_dir(/,/^)/c\FRAMEWORK_SDK_DIR = join(FRAMEWORK_DIR, "tools", "esp32-arduino-libs")' "$FILE_PATH"
+#
+#  # Check if the sed command was successful
+#  if [ $? -eq 0 ]; then
+#    echo "File $FILE_PATH updated successfully."
+#  else
+#    echo "Failed to update the file $FILE_PATH."
+#    exit 1
+#  fi
+#done
 
-  # Check if the sed command was successful
-  if [ $? -eq 0 ]; then
-    echo "File $FILE_PATH updated successfully."
-  else
-    echo "Failed to update the file $FILE_PATH."
-    exit 1
-  fi
-done
-
-cp -rf arduino-esp32/ framework-arduinoespressif32-build/
+cp -rf arduino-esp32/ fae32-build/
 
 # If the framework is needed as tar.gz uncomment next line
 # tar --exclude=.* -zcf ../$pio_archive_path framework-arduinoespressif32/
 mkdir -p ../../dist
-7z a -mx=9 -tzip -xr'!.*' ../../dist/$pio_zip_archive_path framework-arduinoespressif32/
-7z a -mx=9 -tzip -xr'!.*' ../../dist/$pio_zip_archive_libs_path framework-arduinoespressif32-libs/
-7z a -mx=9 -tzip -xr'!.*' ../../dist/$pio_zip_archive_short framework-arduinoespressif32-build/
+7z a -mx=9 -tzip -xr'!.*' ../../dist/$pio_zip_archive_path fae32/
+7z a -mx=9 -tzip -xr'!.*' ../../dist/$pio_zip_archive_libs_path fae32-libs/
+7z a -mx=9 -tzip -xr'!.*' ../../dist/$pio_zip_archive_short fae32-build/
